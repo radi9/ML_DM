8e70e99b8cbaedc5d02daea80cf3a9c6ad2291ba
radi9 <redbutbray@gmail.com>
2014-03-07 21:31:02 +0800

    get user,movie,and user rating record

--- assignment1/preprocess.py
+++ assignment1/preprocess.py
@@ -41,6 +41,20 @@ def accessFile(path,property):
     file = open(path,property)
     return file
 
+def output_record(fileSource,fileObject):
+    count = 0
+    for lines in fileSource :
+        count += 1
+        fileObject.write(str(count)+'\t'+lines+'\n')
+    return fileObject
+
+
+#Get the data property and test here
+
+#write the user and movie list into file for validation or something..
+#if this program logical is correct then the file r1.userID and r1.moveID have the correct data
+
+#next time we could verify the data or use the data by read the file ,it is more shorter than original r1.train data set
 
 _source = '/home/red/ML_DM data/ml-10M/'
 
@@ -54,30 +68,32 @@ write_userList = accessFile(_source+'r1.userID','w')
 
 write_movieList = accessFile(_source+'r1.movieID','w')
 
+write_out_userList = output_record(user_list,write_userList)
 
-#Get the data property and test here
+write_out_userList.close()
 
-#write the user and movie list into file for validation or something..
-#if this program logical is correct then the file r1.userID and r1.moveID have the correct data
+write_out_movieList = output_record(movie_list,write_movieList)
 
-#next time we could verify the data or use the data by read the file ,it is more shorter than original r1.train data set
+write_out_movieList.close()
 
-for lines in user_list :
-    write_userList.write(lines)
-
-for lines in movie_list :
-    write_movieList.write(lines)
     
 print len(user_list) #55682
 print len(movie_list) #10665
 
-print 'this train data size is '+getsizeof(train_data)
-print 'this user list size is '+getsizeof(user_list)
-print 'this movie list size is '+getsizeof(movie_list)
+print 'this train data size is '+str(getsizeof(train_data)) #32208576 bytes
+print 'this user list size is '+str(getsizeof(user_list))   #228676
+print 'this movie list size is '+str(getsizeof(movie_list)) #43812
 
 user_records = getUser_dict(train_data)
 
-print user_records['15555']
+write_user_records = accessFile(_source+'r1user.record','w')
+
+write_out_user_records = output_record(user_records,write_user_records)
+
+write_out_user_records.close()
+
+
+"print user_records['15555']"
 #print NO.15555 user records
 
 #all fucntion is test is ok...but for r1.train ....it's performance bottleneck
